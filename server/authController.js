d7e9032e1a6c7bbb900c8d18b212907133e6c7fc
Jordan Morris <jthecybertinkerer@gmail.com>
2020-09-16 17:56:31 -0500

    creating configuration workaround

--- server/authController.js
+++ server/authController.js
@@ -1,56 +1,50 @@
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs");
 
 module.exports = {
-    register: async(req, res) => {
+  register: async (req, res) => {
+    const db = req.app.get("db"),
+      { email, password } = req.body;
 
-        const db = req.app.get('db'),
-         {email, password} = req.body;
+    console.log(email, "req.body");
+    console.log(password, "pass re.body");
 
-console.log(email, 'req.body')
-console.log(password,'pass re.body')
+    const foundUser = await db.check_user(email);
+    if (foundUser[0]) {
+      return res.status(400).send("Email already registered");
+    }
+    let salt = bcrypt.genSaltSync(),
+      hash = bcrypt.hashSync(password, salt);
 
-         const foundUser = await db.check_user(email)
-       if(foundUser[0]){
-           return res.status(400).send('Email already registered')
-       }
-        let salt = bcrypt.genSaltSync(),
-        hash = bcrypt.hashSync(password, salt);
+    const newUser = await db.register([email, hash]);
 
-        const newUser = await db.register([email, hash])
+    req.session.user = newUser[0];
 
-        req.session.user = newUser[0]
+    res.status(201).send(req.session.user);
+  },
+  getUsers: async (req, res) => {
+    const db = req.app.get("db");
 
-        res.status(201).send(req.session.user)
+    const users = await db.getUsers();
 
-    },
-    getUsers: async(req, res)=>{
-        const db = req.app.get('db');
+    res.status(200).send(users);
+  },
+  login: async (req, res) => {
+    const { email, password } = req.body,
+      db = req.app.get("db"),
+      [foundUser] = await db.check_user(email);
 
-        const users = await db.getUsers()
-        
-        res.status(200).send(users)
-
-
-    },
-    login: async(req, res) =>{
-        const {email, password} = req.body,
-        db = req.app.get('db'),
-
-        foundUser = await db.check_user(email);
-
-        if(!foundUser[0]){
-            res.status(400).send('user not found')
-        }
-
-        const authenticated = bcrypt.compareSync(password, foundUser[0].password)
-        if(!authenticated){
-            return res.status(403).send('Username or password do not match')
-        }
-        delete foundUser[0].password
-        req.session.user = foundUser[0]
-
-        res.status(202).send(req.session.user)
-        
+    if (foundUser == null) {
+      res.status(404).send("user not found");
+      return;
     }
 
-}
+    const authenticated = bcrypt.compareSync(password, foundUser[0].password);
+    if (!authenticated) {
+      return res.status(403).send("Username or password do not match");
+    }
+    delete foundUser[0].password;
+    req.session.user = foundUser[0];
+
+    res.status(202).send(req.session.user);
+  },
+};
